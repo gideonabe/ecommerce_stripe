ae924432b664671d9cb5e0deef3dcb4c834cd68b
Gideon0017 <159183494+Gideon0017@users.noreply.github.com>
2025-01-02 20:04:33 +0100

    Built production-ready Next.js app with updates

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your Next.js configuration options here
+};
+
+module.exports = nextConfig;
+
